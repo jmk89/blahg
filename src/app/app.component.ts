ad87d081d0675795fbd846f0c1a63d0700b07368
jmk <jmkolata@gmail.com>
2021-12-15 13:03:48 -0500

    added bootstrap, simple homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Blahg';
+  posts = [
+    { name: "Post 1", content: "the first few words", date: new Date("2021-12-01") },
+    { name: "Post 2", content: "some intro words", date: new Date("2021-12-02") },
+    { name: "Post 3", content: "a hot take woah", date: new Date("2021-12-03") },
+    { name: "Post 4", content: "testing waters", date: new Date("2021-12-04") },
+  ]
+
+
 }
