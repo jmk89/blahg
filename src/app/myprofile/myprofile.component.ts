f2a07649da384f5314ac32224b78cf75dd31bec2
jmk <jmkolata@gmail.com>
2021-12-16 15:47:21 -0500

    user is able to see the overview of their blahgs

--- src/app/myprofile/myprofile.component.ts
+++ src/app/myprofile/myprofile.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './../user/user.model';
+import { PostService, PostData, JankyFirebasePostData } from './../services/post.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-myprofile',
   templateUrl: './myprofile.component.html',
   styleUrls: ['./myprofile.component.css']
 })
-export class MyprofileComponent implements OnInit {
+export class MyprofileComponent implements OnInit, OnDestroy {
+  postsObservable: Observable<JankyFirebasePostData>;
+  posts: PostData[] = [];
 
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    const user: User = JSON.parse(localStorage.getItem('userData'));
+    this.postsObservable = this.postService.getPostsForUserID(user.id);
+    this.postsObservable
+      .subscribe(response => {
+        //some jank from
+        //https://stackoverflow.com/questions/63293324/how-to-turn-a-firebase-rest-api-response-into-an-array-in-typescript
+        //to help
+        for (const [key, value] of Object.entries(response)) {
+          this.posts.push(value as PostData)
+        }
+        console.log(this.posts)
+      })
+  }
+
+  ngOnDestroy(): void {
+      
   }
 
 }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,11 +1,19 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, tap, throwError } from 'rxjs';
+import { catchError, map, tap, throwError } from 'rxjs';
 
 export interface PostData {
     userID: string,
     title: string,
     body: string
+}
+
+export interface JankyFirebasePostData {
+    root: [{
+        [key: string]: {
+            post: PostData
+        }
+    }]
 }
 
 @Injectable({providedIn: 'root'})
@@ -25,6 +33,19 @@ export class PostService {
                 console.log(response)
             })
         )
+    }
+
+    getPostsForUserID(userID: string) {
+        return this.http.get<JankyFirebasePostData>(
+            'https://blahg-5b828-default-rtdb.firebaseio.com/posts.json'
+        ).pipe(
+            tap(posts => {
+                // let j: JankyFirebasePostData = posts
+                // const s = JSON.stringify(j);
+                // const js = JSON.parse(s);
+                // console.log(js);
+            })
+        );
     }
 
     private handleError(errorResponse: HttpErrorResponse) {
