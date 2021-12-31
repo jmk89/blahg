9eff2a3bc565d4f6c648415d51953fcfbf2da3be
jmk <jmkolata@gmail.com>
2021-12-31 13:11:52 -0500

    user prefs info shows up in their profile

--- src/app/user-profile/myprofile.component.ts
+++ src/app/user-profile/myprofile.component.ts
@@ -1,33 +1,39 @@
-import { FirestorePostService, PostData2 } from './../shared/services/firestorePost.service';
+import {
+  FirestorePostService,
+  PostData2,
+} from './../shared/services/firestorePost.service';
 import { Observable } from 'rxjs';
 import { AuthUser } from '../shared/models/auth-user.model';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FirebaseUser } from '../shared/models/firebase-user.model';
+import { UserPreferencesData, UserPreferencesService } from '../shared/services/user-preferences.service';
 
 @Component({
   selector: 'app-myprofile',
   templateUrl: './myprofile.component.html',
-  styleUrls: ['./myprofile.component.css']
+  styleUrls: ['./myprofile.component.css'],
 })
 export class MyprofileComponent implements OnInit, OnDestroy {
   posts$: Observable<PostData2[]>;
   posts: PostData2[] = [];
+  prefs: UserPreferencesData
 
   constructor(
-    private firestorePostService: FirestorePostService
-  ) { }
+    private firestorePostService: FirestorePostService,
+    private userPrefs: UserPreferencesService
+  ) {}
 
   ngOnInit(): void {
-    const user: FirebaseUser = JSON.parse(localStorage.getItem('firebaseUserData'));
-    this.posts$ = this.firestorePostService.getUserPosts(user.uid)
-    this.posts$
-      .subscribe(res => {
-        this.posts = res;
-      })
+    const user: FirebaseUser = JSON.parse(
+      localStorage.getItem('firebaseUserData')
+    );
+    this.posts$ = this.firestorePostService.getUserPosts(user.uid);
+    this.posts$.subscribe((res) => {
+      this.posts = res;
+    });
+
+    this.prefs = <UserPreferencesData>JSON.parse(localStorage.getItem('userPreferences'));
   }
 
-  ngOnDestroy(): void {
-
-  }
-
+  ngOnDestroy(): void {}
 }
